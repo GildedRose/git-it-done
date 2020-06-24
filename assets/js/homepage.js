5ab380bf6e00fe19d1280da6164be4210faa603f
Anne <anne.k.plummer@gmail.com>
2020-06-24 17:20:07 -0500

    repo data displayed with for loop, repo issues marked using if else

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,3 +1,6 @@
+var repoContainerE1 = document.querySelector("#repos-container");
+var repoSearchTerm = document.querySelector("#repo-search-term");
+
 var getUserRepos = function(user) {
     //format the github api url
     var apiUrl = "https://api.github.com/users/" + user + "/repos";
@@ -5,7 +8,7 @@ var getUserRepos = function(user) {
     //make a request to the url
     fetch(apiUrl).then(function(response) {
         response.json().then(function(data) {
-            console.log(data);
+            displayRepos(data, user);
         });
     });
 };
@@ -26,6 +29,46 @@ var formSumbitHandler = function(event) {
         alert("Please enter a GitHub username");
     }
     console.log(event);
+};
+
+var displayRepos = function(repos, searchTerm) {
+    repoContainerE1.textContent = "";
+    repoSearchTerm.textContent = searchTerm;
+
+    //loop over repos
+    for (var i = 0; i < repos.length; i++) {
+        //format repo name
+        var repoName = repos[i].owner.login + "/" + repos[i].name;
+
+        //create a container for each repo
+        var repoE1 = document.createElement("div");
+        repoE1.classList = "list-item flex-row justify-space-between align center";
+
+        // create a span element to hold repository name
+        var titleE1 = document.createElement("span");
+        titleE1.textContent = repoName;
+
+        // append to container
+        repoE1.appendChild(titleE1);
+
+        // create a status element
+        var statusE1 = document.createElement("span");
+        statusE1.classList = "flex-row align cetner";
+
+        // check if current repo has issues or not
+        if (repos[i].open_issues_count > 0){
+            statusE1.innerHTML =
+            "<i class='fas fa-times status-icon icon-danger'></i>" + repos[i].open_issues_count + "issue(s)";
+        } else {
+            statusE1.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>";
+        }
+        
+        // append to container
+        repoE1.appendChild(statusE1);
+
+        // append container to dom
+        repoContainerE1.appendChild(repoE1);
+    }
 };
 
 userFormE1.addEventListener("submit", formSumbitHandler);
